227d0d52aab79d2700e3af4ffc0676f3d09da0b6
Pixel <pixel@webmail.com>
2025-03-02 02:01:42 +0300

    Fixed index lock issue

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,11 +1,10 @@
-
 from django.urls import path
 from . import views
+from . import apps
+
 urlpatterns = [
-    path('', views.index),
-    path('index2/<int:val1>/', views.index2),
-    path('<int:bookId>', views.viewbook)
+    path('', views.index, name="bookmodule.index"),  # الصفحة الرئيسية
+    path('list_books/', views.list_books, name="bookmodule.list_books"),
+    path('aboutus/', views.aboutus, name="bookmodule.aboutus"),
+    path('<int:bookId>/', views.viewbook, name="bookmodule.view_one_book"),  # وضعه في النهاية
 ]
-
-
-
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,14 +19,7 @@ from django.contrib import admin
 from django.urls import path
 import apps.bookmodule.views 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),  
-    path('', apps.bookmodule.views.index),
-    path('index2/<int:val1>/', apps.bookmodule.views.index2)  
-]
-urlpatterns += [
-    path('index2/<int:val1>/', apps.bookmodule.views.index2)
-]
+
 
 from django.urls import include, path
 urlpatterns = [
